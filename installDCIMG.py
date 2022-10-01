21390c2ee23f74a9ecd71e4e76536d9658a631e6
scottkil <32939989+scottkil@users.noreply.github.com>
2022-10-01 08:16:58 -0400

    Update installDCIMG.py

--- installDCIMG.py
+++ installDCIMG.py
@@ -1,11 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
+# basic script to that new users can run to install the dcimg package for later use through MATLAB
+# dcimg is a package used to work with .dcimg files that are created when imaging with Hamamatsu cameras
+# More info on dcimg found here: https://lens-biophotonics.github.io/dcimg/
 
-This is a temporary script file.
-"""
 import sys
 import subprocess
 
-# implement pip as a subprocess:
+# implement pip as a subprocess and install dcimg package
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'dcimg'])
